e153bb59ed9aea89002120752322735e5cfd1011
Jakob Möllås <jakob.mollas@gmail.com>
2021-08-28 19:42:46 +0200

    refactor

--- src/text.js
+++ src/text.js
@@ -1,19 +1,19 @@
+const fontWidth = 700;
+const fontName = "Hind";
+
 export function TextToPoints(canvas, text, pixelsPerPoint, stageWidth, stageHeight) {
     const ctx = canvas.getContext("2d");
 
     canvas.width = stageWidth;
     canvas.height = stageHeight;
 
-    const fontWidth = 700;
-    const fontName = "Hind";
     let fontSize = 10;
     let metrics;
 
-    ctx.clearRect(0, 0, stageWidth, stageHeight);
     ctx.fillStyle = "rgba(0, 0, 0, 1)";
     ctx.textBaseline = "middle";
 
-    // find a good size for the text
+    // find a good size for the text based on canvas size
     while (true) {
         ctx.font = `${fontWidth} ${fontSize}px ${fontName}`;
         metrics = ctx.measureText(text);
@@ -24,6 +24,7 @@ export function TextToPoints(canvas, text, pixelsPerPoint, stageWidth, stageHeig
     }
 
     // write text - the resulting image will be used to calculate points
+    ctx.clearRect(0, 0, stageWidth, stageHeight);
     ctx.fillText(
         text,
         (stageWidth - metrics.width) / 2,
@@ -32,14 +33,14 @@ export function TextToPoints(canvas, text, pixelsPerPoint, stageWidth, stageHeig
         ((stageHeight - fontSize) / 2)
     );
 
-    return createPoints(ctx, pixelsPerPoint);
+    return createPointsFromImage(ctx, pixelsPerPoint);
 }
 
-function createPoints(ctx, pixelsPerPoint) {
-    const points = [];
+function createPointsFromImage(ctx, pixelsPerPoint) {
     const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height).data;
 
     // sample image data and create points for pixels with data
+    const points = [];
     for (let height = 0; height < ctx.canvas.height; height += pixelsPerPoint) {
         for (let width = 0; width < ctx.canvas.width; width += pixelsPerPoint) {
             const pixelAlpha = imageData[((width + (height * ctx.canvas.width)) * 4) - 1];
