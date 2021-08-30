5695c7c8ec37e261f25e9d0b764b977e397ad9db
Jakob Möllås <jakob.mollas@gmail.com>
2021-08-30 11:52:47 +0200

    update settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 export class Settings {
     showDiagnostics: boolean = false;
-    density: number = 3;
-    text: string = "YUM";
+    density: number = 4;
+    text: string = "♥";
     viscosity: number = 0.9;
     liveliness: number = 0.5;
     pointerRadius: number = 100 / (window.devicePixelRatio ?? 1);
